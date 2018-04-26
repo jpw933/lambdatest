f5620a26fcf14e0af9fbc0e498132a26cdaa3e81
Jianping Wei <jwei@truecar.com>
2018-04-26 15:41:06 -0700

    Test1 Test1 Test1

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ var AWS = require('aws-sdk');
 
 exports.handler = function(event, context, callback) {  
   var bucketName = process.env.S3_BUCKET;       
-
+  console.log("Test");
   callback(null, bucketName);     
-
 }
 
